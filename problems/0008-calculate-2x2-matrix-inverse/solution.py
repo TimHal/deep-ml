3eaecf5ce469f79f4cfe27b13ec2367a34e39654
TimHal <12948708+TimHal@users.noreply.github.com>
2026-09-24 21:20:33 +0000

    Solve: Calculate 2x2 Matrix Inverse

---
+++ problems/0008-calculate-2x2-matrix-inverse/solution.py
@@ -0,0 +1,23 @@
+def inverse_2x2(matrix: list[list[float]]) -> list[list[float]] | None:
+    """
+    Calculate the inverse of a 2x2 matrix.
+    
+    Args:
+        matrix: A 2x2 matrix represented as [[a, b], [c, d]]
+    
+    Returns:
+        The inverse matrix as a 2x2 list, or None if the matrix is singular
+        (i.e., determinant equals zero)
+    """
+    # Check the determinants of S, T to assert their invertibility
+    def det2d(matrix: list[list[int|float]]) -> float:
+        return matrix[0][0]*matrix[1][1] - matrix[1][0]*matrix[0][1] 
+
+    det = det2d(matrix)
+    if det == 0:
+        return None
+
+    matrix_inv = [[1/det * matrix[1][1], -1/det * matrix[0][1]], [-1/det * matrix[1][0], 1/det * matrix[0][0]]]
+
+    return matrix_inv
+	
